44c8ddd76036fe4cbf7ab1699f1baad8f360c3f8
Guigas <guilherme@catral.com.br>
2021-08-26 17:33:01 -0300

    Criação da pagina de Sala
    Foi criada a pagina de sala onde esta disposto o local da criação da pergunta, botão de Enviar e de logar caso necessario

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,5 +1,45 @@
+import { useParams } from 'react-router-dom';
+import logoImg from '../assents/logo.svg';
+import { Button } from '../componets/Button';
+import { RoomCode } from '../componets/RoomCode';
+
+import '../styles/room.scss'
+
+type RoomParams = {
+    id: string;
+}
+
 export function Room() {
+    const params = useParams<RoomParams>();
+
+
+
     return(
-        <h1>Sala</h1>
+        <div id="page-room">
+            <header>
+               <div className="content">
+                   <img src={logoImg} alt = "Letmeask" />
+                   <RoomCode code={params.id}/>
+               </div>
+            </header>
+
+            <main>
+              <div className="room-title">
+                <h1>Sala React</h1>
+                <span>4 perguntas</span>
+              </div> 
+             
+             <form>
+                 <textarea
+                 placeholder="O que vc deseja perguntar?"
+                 />
+
+              <div className="form-footer">
+                  <span> Para enviar uma pergunta,<button>faça seu loguin</button>.</span>
+                  <Button type="submit">Enviar Pergunta</Button>
+              </div>
+             </form>
+           </main>            
+        </div>
     );
 }
